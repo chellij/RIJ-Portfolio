c4700e733cb49423de82ad92000bcf702e0eb775
Rachel Ireland-Jones <74239304+chellij@users.noreply.github.com>
2021-02-23 12:46:13 +0000

    Update Arduino Joystick Code.ino

--- 2. Arduino Joystick Programming/Arduino Joystick Code.ino
+++ 2. Arduino Joystick Programming/Arduino Joystick Code.ino
@@ -1,13 +1,15 @@
 
 // Fan and Joystick
-#define analogX A0 // Analog input pin that the potentiometer is attached to
-#define analogY A1
-#define JoyXCentre 496
+#define analogX A0    // Analog input pin Joystick X direction
+#define analogY A1    // Analog input pin Joystick Y direction
+#define JoySwitch 13  // Analog input pin Joystick Button
+#define MotorA 6      // Input 1 from Arduino to DC Motor via L293D Motor Driver
+#define MotorB 5      // Input 2 from Arduino to DC Motor via L293D Motor Driver
+
+#define ENDriver 7    // L293D Motor Driver Enable Pin
+
+#define JoyXCentre 496  // analog readings for X and Y when joystick is in centre
 #define JoyYCentre 511
-#define JoySwitch A2
-#define ENDriver 3
-#define MotorA 6
-#define MotorB 5
 
 int valX = analogRead(analogX); 
 int valXneg = analogRead(analogX);
@@ -16,20 +18,20 @@ int valYneg = analogRead(analogY);
 int JoyX = 0; 
 int JoyY = 0;  
 
+
 //Servo
-#define ServoSignal 4
+#define ServoSignal 4   // Servo Control Signal 
 int on;
 int off;
 
-//Stepper Motor
-#define StepperCoilA 8
-#define StepperCoilB 9
-#define StepperCoilC 10
+//Stepper Motor Controller
+#define StepperCoilA 8 
+#define StepperCoilB 9  
+#define StepperCoilC 10 
 #define StepperCoilD 11
 
-
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{
   Serial.begin(9600);
   pinMode(MotorA, OUTPUT);
   pinMode(MotorB, OUTPUT);
@@ -48,9 +50,8 @@ void pos()
   Serial.println(deg);
 }
 
-
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
   valX = map(valX, 0, 515, 255, 0);
   valXneg = map(valXneg, 519, 1023, 0, 255);
   
@@ -76,14 +77,6 @@ void loop() {
     //Drive Motor one direction
     digitalWrite(MotorA, HIGH);
     digitalWrite(MotorB, LOW);
-
-//    pos();
-//    on += 111;
-//    off-= 111;
-//    digitalWrite(ServoSignal, HIGH);
-//    delayMicroseconds(on);
-//    digitalWrite(ServoSignal, LOW);   
-//    delayMicroseconds(off);
   }
 
   else if (JoyX > JoyXCentre+5)
@@ -93,13 +86,6 @@ void loop() {
     delayMicroseconds(off);
     digitalWrite(MotorB, HIGH);
     delayMicroseconds(on);
-
-
-//    pos();
-//    on -= 111;
-//    off+= 111;
-//    digitalWrite(ServoSignal, HIGH);
-//    digitalWrite(ServoSignal, LOW);
   }
 
   else
